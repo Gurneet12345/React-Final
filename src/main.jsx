06871644e4e0dfca4672633201cd2be3b3937cc7
Gurneet12345 <gurneet1.singh@student.vcc.ca>
2024-12-11 23:50:21 -0800

    created character home and character pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
-<<<<<<< HEAD
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-=======
-=======
->>>>>>> origin/Gurneet
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
@@ -21,9 +10,5 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-<<<<<<< HEAD
->>>>>>> origin/Gurneet
-=======
->>>>>>> origin/Gurneet
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 // https://vite.dev/config/
-=======
+
 // https://vitejs.dev/config/
->>>>>>> origin/Gurneet
-=======
+
+
 // https://vitejs.dev/config/
->>>>>>> origin/Gurneet
+
 export default defineConfig({
   plugins: [react()],
 })
